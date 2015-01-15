f98e6ccfc3562ffeb99adf3c5b84e936bbd484d0
Michael Meyer <m.meyer2k@gmail.com>
2015-01-15 11:56:56 -0600

    Some fixes and comments

--- phporacle.php
+++ phporacle.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This is a PHP POC of a padding oracle attack. It works with local exceptions
  * instead of interacting with a server for the sake of simplicity.
@@ -17,31 +18,39 @@ require 'badHash.php';
 $key = str_repeat('A', 32);
 
 // Create the secret that we want to crack...
-$secret = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbcc';
+// The first 32 byte block will always be undecryptable
+//         |--- will decrypt to garbage --|
+$secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXabcdefghijklmnopqrstuvwxyz';
 $encrypted = badCrypt::encrypt($secret, $key);
 
 $blocks = str_split($encrypted, 32);
+
+// Count the numner of blocks
+// The first 2 blocks are always IV + checksum
 $count = count($blocks);
 $head = $blocks[0] . $blocks[1];
 
-// Begin looping through the blocks of cypher text
+// Create a place holder array for decrypted blocks
+$secretblocks = array();
+
+// Begin looping through the blocks of cypher text in reverse order
 // Ignore the first 2 blocks, because those are the IV and check sum
 // used by the encryption library.
-for ($nblock = $count - 1; $nblock > 2; $nblock--) {
-    
+for ($nblock = $count - 1; $nblock > 1; $nblock--) {
+
     // Create a single 32 byte block of FF bytes
     $badblock = str_repeat("\xFF", 32);
-    
+
     // Secret block will hold our stolen information
     $secretblock = array();
-    
+
     // Lets start decoding from the last byte first
     for ($nbyte = 31; $nbyte >= 0; $nbyte--) {
-        
+
         // Begin looping through every possible value for the byte
         for ($tbyte = 0; $tbyte < 256; $tbyte++) {
             $badblock[$nbyte] = chr($tbyte);
-            
+
             // Here is where the magic happens...
             // Bad padding will result in an exception which is caught and skipped
             try {
@@ -51,29 +60,35 @@ for ($nblock = $count - 1; $nblock > 2; $nblock--) {
             }
 
             // Create a position pointer which is an offset from the end of the
-            // block of the byte we are operating on.
-            $pos = 32 - $nbyte;
+            // block of the byte we are operating on. This integer corresponds
+            // to the amount of padding that MUST be being used for the oracle attack
+            $padlen = 32 - $nbyte;
 
-            // Here we will grab the byte of cypher text which we are operating on
-            $derp = ord($blocks[$nblock - 1][$nbyte]);
-            
+            // Here we will grab the byte of cypher text in the block before our
+            // target block.
+            $prev = ord($blocks[$nblock - 1][$nbyte]);
+
             // Decrypt the stolen byte
-            $stolen = $pos ^ $derp ^ $tbyte;
-            
+            $stolen = $padlen ^ $prev ^ $tbyte;
+
             // Add stolen byte to the block of secrets
             $secretblock[$nbyte] = $stolen;
 
             // Loop through the previous bytes of the bad block to fix each
             // byte for the next round.
             for ($fixpos = 31; $fixpos >= $nbyte; $fixpos--) {
-                $new = ($pos + 1) ^ $secretblock[$fixpos] ^ ord($blocks[$nblock - 1][$fixpos]);
+                $new = ($padlen + 1) ^ $secretblock[$fixpos] ^ ord($blocks[$nblock - 1][$fixpos]);
                 $badblock[$fixpos] = chr($new);
             }
 
             break;
         }
     }
-    
-    echo bin2hex(implode($secretblock));
-    
+
+    // Reverse the block
+    $secretblock = array_reverse($secretblock);
+
+    foreach ($secretblock as $s) {
+        echo chr($s);
+    }
 }
